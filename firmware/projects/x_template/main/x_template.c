cf7a05917f10151e521bdce5a6ad26343f0a6adb
lucianacab15 <lucianacab15@gmail.com>
2025-08-06 17:50:43 -0300

    actividad 2 de la guia 1

--- firmware/projects/x_template/main/x_template.c
+++ firmware/projects/x_template/main/x_template.c
@@ -26,7 +26,13 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
 /*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD 200
 
 /*==================[internal data definition]===============================*/
 
@@ -34,6 +40,26 @@
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	uint8_t teclas;
+	LedsInit();
+	SwitchesInit();
+    while(1)    {
+    	teclas  = SwitchesRead();
+    	switch(teclas){
+    		case SWITCH_1:
+    			LedToggle(LED_1);
+    		break;
+    		case SWITCH_2:
+    			LedToggle(LED_2);
+    		break;
+			case SWITCH_1 | SWITCH_2:
+				LedToggle(LED_3);
+			break;
+			default:
+				LedsOffAll();
+			break;
+    	}
+		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+	}
 }
 /*==================[end of file]============================================*/
